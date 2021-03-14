b6659a3f15ce18a0f0e7126eb12e17b4b36bd415
Zhi-Qiang You <zyou@osc.edu>
2021-03-14 18:04:38 -0400

    Add usage update script and remove old usage files

--- update_usage.sh
+++ update_usage.sh
@@ -1,36 +1,41 @@
 #!/bin/bash
 
-mlen=('xx' '31' '28' '31' '30' '31' '30' '31' '31' '30' '31' '30' '31')
+today=$(date --date='today' +"%Y%m%d")
 
-if [ $# -lt 4 ]; then
-  echo "Usage: $0 year month system <pbs|xalt>"
-  exit -1
-fi
+update_usage() {
+  startdate=$1
+  enddate=$2
+  echo Update software usage from $startdate to $enddate
+  sbatch --account=PZS0710 --nodes=1 --exclusive \
+         --time=60 \
+         --job-name="xalt-usage-update-$today" \
+         --output=/fs/ess/PZS0710/database/xalt/logs/update-usage-${startdate}-${enddate}@${today}-%j.log \
+         --mail-user=zyou@osc.edu \
+         --mail-type=FAIL \
+         --export=startdate=${startdate},enddate=${enddate} \
+         update_usage.slurm
+}
 
 year=$1
 month=$2
-system=$3
-database=$4
-logger=stdout
 
-fh=`printf "%d%02d" $year $month`
-start_t=`printf "%d-%02d-01" $year $month`
-end_t=`printf "%d-%02d-%d" $year $month ${mlen[$month]}`
-f_month=`printf "%02d" $month`
-
-logfile="usage/${database}/${fh}${system}.log"
-tmpfile="usage/${database}/${fh}${system}.log.tmp"
-
-if [ "$database" = "pbs" ]; then
-  echo ./pbsacct_usage_report.py --start $start_t --end $end_t --num 50 --syshost $system --log $logger
-# ./pbsacct_usage_report --start $start_t --end $end_t --num 50 --syshost $system --log $logger |grep ^syshost |tee usage/pbs/${fh}${system}.log
-  ./pbsacct_usage_report --start $start_t --end $end_t --num 50 --syshost $system --log $logger |& tee $tmpfile
+if [ "x$year" = "x" ] || [ "x$month" = "x" ]; then
+  l_startdate=$(date --date="last month" +"%Y-%m-01")
+  l_enddate=$(date --date="$l_startdate + 1 month -1 day" +"%Y-%m-%d")
+  c_startdate=$(date --date="1 days ago" +"%Y-%m-01")
+  [ "$c_startdate" = "$l_startdate" ] && c_startdate=""
+  c_enddate=$(date --date="1 days ago" +"%Y-%m-%d")
+  # No usage update for previous month after day 15
+  [ $(date +"%d") -gt 15 ] && l_startdate=""
+else
+  c_startdate=$(date --date="$year-$month-01" +"%Y-%m-%d")
+  c_enddate=$(date --date="$c_startdate + 1 month -1 day" +"%Y-%m-%d")
 fi
 
-if [ "$database" = "xalt" ]; then
-  echo ./xalt_usage_report.py --start $start_t --end $end_t --num 50 --syshost $system --log $logger
-  ./xalt_usage_report --start $start_t --end $end_t --num 50 --syshost $system --log $logger |& tee $tmpfile
+if [ -n "$l_startdate" ] && [ -n "$l_enddate" ]; then
+  update_usage $l_startdate $l_enddate
 fi
 
-grep ^syshost $tmpfile |while read x; do echo $x year=$year month=$f_month; done |tee $logfile
-test -f $tmpfile && rm -f $tmpfile
+if [ -n "$c_startdate" ] && [ -n "$c_enddate" ]; then
+  update_usage $c_startdate $c_enddate
+fi
--- usage/README.md
+++
@@ -1,4 +0,0 @@
-# Upload montly PBS and XALT software usage 
-```
-ls */*.log |while read x; do logger -t sw_usage -f $x; done
-```
